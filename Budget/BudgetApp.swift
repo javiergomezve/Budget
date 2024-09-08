74f889eaa4342bf2a551adf9f15cb4dd5770ede5
Javier Gomez <javiergomezve@gmail.com>
2024-09-08 11:27:32 -0500

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Javier Gomez on 8/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
